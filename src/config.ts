ace063d8c6a8a1be2f115fa4195800e64b51bbfe
Micheal Mac Donnacha <micheal.macdonnacha13@gmail.com>
2024-10-15 21:31:13 +0100

    update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,8 +1,8 @@
 import type { Site, SocialObjects } from "./types";
 
 export const SITE: Site = {
-  website: "https://astro-paper.pages.dev/", // replace this with your deployed domain
-  author: "Sat Naing",
+  website: "https://mmacdonnacha.github.io/astro-test/", // replace this with your deployed domain
+  author: "Michael",
   profile: "https://satnaing.dev/",
   desc: "A minimal, responsive and SEO-friendly Astro blog theme.",
   title: "AstroPaper",
